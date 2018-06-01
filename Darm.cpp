0abb6f680912cbf7602786de86728adf121ef952
joeri <joeri.temmerman@student.ap.be>
2018-06-01 11:19:32 +0200

    fixing point to angle conversion logic

--- Darm.cpp
+++ Darm.cpp
@@ -29,7 +29,7 @@ DArm::DArm(double lengthN, double lengthF, Point A, Point B){
 }
 
 Point DArm::Convert(Point point){
-	Serial.println("enter Convert()");
+	//Serial.println("enter Convert()");
 	Point ret;
 	PointTuple intA;
 	PointTuple intB;
@@ -38,10 +38,13 @@ Point DArm::Convert(Point point){
 
 	// determine correct ones
 	// i'm thinking a is correct for A, b is correct for B?
-	ret.x = tanh(intA.a.y/intA.a.x) / 2 / PI * 360;
-	ret.y = tanh(intB.b.y/(intB.b.x - this->B.x)) / 2 / PI * 360;
+
+	//calculate angle
+	ret.x = atan(intA.a.y/intA.a.x) / 2 / PI * 360;
+	ret.y = atan(intB.b.y/(intB.b.x - this->B.x)) / 2 / PI * 360;
+
 	Serial.print("retx = ");
-	Serial.print(ret.x);
+	Serial.print(ret.x, 3);
 	Serial.print("rety = ");
 	Serial.println(ret.y, 3);
 	return ret;
@@ -56,33 +59,44 @@ PointTuple DArm::CalcIntersects(Point A, double Ar, Point B, double Br){
 	// calculate/ check d
 	// d = ||P1 - P0||
 	// if d > R0 + R1 => nope
-	Serial.println("Start Calcintersects()");
+	//Serial.println("Start Calcintersects()");
+	//Serial.print("pointA ");
+	//printPoint(A);
+	//Serial.println("");
+	//Serial.print("pointB ");
+	//printPoint(B);
+	//Serial.println("");
+	//Serial.print("Ar ");
+	//Serial.print(Ar, DEC);
+	//Serial.print(", Br ");
+	//Serial.print(Br, DEC);
+	//Serial.println();
 	double d = sqrt(pow((A.x - B.x), 2) + pow((A.y - B.y), 2));
-	Serial.print("d = ");
-	Serial.println(d, 3);
+	//Serial.print("d = ");
+	//Serial.println(d, 3);
 	if(d > (Ar + Br)){
 		return PointTuple{Point{-1, -1}, Point{-1, -1}}; // ERROR
 	}
 
 	// a = (r0^2 - r1^2 + d^2 ) / (2 d)
 	double a = (Ar*Ar - Br*Br + d*d) / (2 * d);
-	Serial.print("a = ");
-	Serial.println(a, 3);
+	//Serial.print("a = ");
+	//Serial.println(a, 3);
 
 	//h2 = r0^2 - a^2
 	double h = sqrt(Ar*Ar - a*a);
-	Serial.print("h = ");
-	Serial.println(h, 3);
+	//Serial.print("h = ");
+	//Serial.println(h, 3);
 
 	// calculate XY2
 	// P2 = P0 + a ( P1 - P0 ) / d
 	Point C; // the centre point between A and B
 	C.x = A.x + a*(B.x - A.x) / d;
 	C.y = A.y + a*(B.y - A.y) / d;
-	Serial.print("C.x = ");
-	Serial.println(C.x, 3);
-	Serial.print("C.y = ");
-	Serial.println(C.y, 3);
+	//Serial.print("C.x = ");
+	//Serial.println(C.x, 3);
+	//Serial.print("C.y = ");
+	//Serial.println(C.y, 3);
 
 	PointTuple R; // resulting tuple of points
 	// X3 = X2 +/- h (Y1 - Y0)/d
@@ -93,8 +107,8 @@ PointTuple DArm::CalcIntersects(Point A, double Ar, Point B, double Br){
 	Serial.print("R.b.x = ");
 	Serial.println(R.b.x, 3);
 
-	R.a.y = C.y + h*(B.x - A.x) / d;
-	R.b.y = C.y - h*(B.x - A.x) / d;
+	R.a.y = C.y - h*(B.x - A.x) / d;
+	R.b.y = C.y + h*(B.x - A.x) / d;
 	Serial.print("R.a.y = ");
 	Serial.println(R.a.y, 3);
 	Serial.print("R.b.y = ");
@@ -132,6 +146,11 @@ void DArm::MoveTo(Point point){
 	MotorB.run();
 	Serial.println("running");
 	while((MotorA.distanceToGo() + MotorB.distanceToGo()) > 0){
+		Serial.print("steps to go A:");
+		Serial.print(MotorA.distanceToGo(), DEC);
+		Serial.print(", B: ");
+		Serial.print(MotorB.distanceToGo(), DEC);
+		Serial.println();
 		MotorA.run();
 		MotorB.run();
 		// delay(1);
--- DrawingArm.ino
+++ DrawingArm.ino
@@ -2,10 +2,11 @@
 #include "DArm.hpp"
 //The setup function is called once at startup of the sketch
 
-DArm DA(133.0, 133.0, Point{0, 0}, Point{100, 0});
+DArm* DA;
 void setup()
 {
 	Serial.begin(115200);
+	DA = new DArm(140.0, 140.0, Point{0, 0}, Point{100, 0});
 	Serial.println("setup done");
 // Add your initialization code here
 }
@@ -13,38 +14,39 @@ void setup()
 // The loop function is called in an endless loop
 void loop()
 {
+	/*Serial.println("enter char to continue");
 	while(!Serial.available()){
-		Serial.print("in loop");
 	}
-	Serial.read();
+	char in = Serial.read();
+	*/
 
 	Serial.print("startpos: ");
-	Serial.print(DA.MotorA.currentPosition());
+	Serial.print(DA->MotorA.currentPosition());
 	Serial.print(", ");
-	Serial.print(DA.MotorA.currentPosition());
-	Serial.println();
+	Serial.print(DA->MotorA.currentPosition());
+	Serial.println("");
 /*
 	Serial.print("move to ");
-	DA.printPoint({25, 200});
-	DA.MoveTo({25, 200});
+	DA->printPoint({25, 200});
+	DA->MoveTo({25, 200});
 	Serial.print("move to ");
-	DA.printPoint({50, 200});
-	DA.MoveTo({50, 200});
+	DA->printPoint({50, 200});
+	DA->MoveTo({50, 200});
 	Serial.print("move to ");
-	DA.printPoint({50, 225});
-	DA.MoveTo({50, 225});
+	DA->printPoint({50, 225});
+	DA->MoveTo({50, 225});
 	Serial.print("move to ");
-	DA.printPoint({25, 225});
-	DA.MoveTo({25, 225});
+	DA->printPoint({25, 225});
+	DA->MoveTo({25, 225});
 */
 	Serial.print("move to ");
-	DA.printPoint({0, 200});
-	DA.MoveTo({0, 200});
+	DA->printPoint({0, 200});
+	DA->MoveTo({0, 200});
+	Serial.println("");
 	Serial.print("move to ");
-	DA.printPoint({75, 200});
-	DA.MoveTo({75, 200});
+	DA->printPoint({75, 200});
+	DA->MoveTo({75, 200});
 
-
-
+	delay(1000);
 }
 
